a5314ed87883c298b60b75901f043955763fadd4
happycooldude <pieterhuisman2000@gmail.com>
2019-12-10 11:15:47 +0100

    Lab 3 buttons afgemaakt

---
+++ Lab 3 buttons/script.js
@@ -0,0 +1,31 @@
+function createButton(customID, backgroundcolor, buttontext){
+    var button = document.createElement("button");
+
+    if (customID){
+        button.id = customID;
+    }
+    else{
+        button.id = "presetid"
+    }
+
+    button.innerHTML = buttontext;
+    button.style.backgroundColor = backgroundcolor;
+    document.getElementById("container").appendChild(button);
+}
+
+createButton("btn1", "green", "button 1");
+createButton("btn2", "red", "button 2");
+createButton("btn3", "blue", "button 3");
+
+
+document.getElementById("btn1").onclick = function button1(){
+    document.getElementsByClassName("bgc")[0].style.backgroundColor = "green";
+};
+
+document.getElementById("btn2").onclick = function button2(){
+    document.getElementsByClassName("bgc")[0].style.backgroundColor = "red";
+};
+
+document.getElementById("btn3").onclick = function button3(){
+    document.getElementsByClassName("bgc")[0].style.backgroundColor = "blue";
+};
